8d2277b0724e5d226b374f9f8349fa949eac7649
Asal <pamela.m1984@gmail.com>
2019-07-22 20:10:27 -0600

    Sleeping Sound Project
    Initial Commit

---
+++ New Unity Project (5)/Assets/Focus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+   public void changeToFocusScene(int FocusScenceSong)
+    {
+        Application.LoadLevel(FocusScenceSong);
+    }
+}
