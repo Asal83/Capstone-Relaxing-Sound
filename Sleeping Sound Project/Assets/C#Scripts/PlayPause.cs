42bfb20b06a9323620abcec224f2b2dbeea5b78f
Asal <pamela.m1984@gmail.com>
2019-09-08 13:56:28 -0600

    Add Play and pause toggle

---
+++ Sleeping Sound Project/Assets/C#Scripts/PlayPause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayPause : MonoBehaviour
+{
+    public Text MyText;
+    bool status = true;
+    public VideoPlayer VideoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = true;
+    }
+    public void PlayAndPause()
+    {
+        status = !status;
+        if (status == true)
+        {
+            MyText.text = "Play";
+            VideoPlayer.Play();
+        }
+        else
+        {
+            MyText.text = "Pause";
+            VideoPlayer.Pause();
+        }
+    }
+
+
+
+        
+
+
+
+
+
+
+
+
+    
+   
+    void Update()
+    {
+        
+    }
+}
